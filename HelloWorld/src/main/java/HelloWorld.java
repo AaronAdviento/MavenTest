60d907e58db8c237e7eed7150c96ea7a114a3c9e
AaronAdviento <113593827+AaronAdviento@users.noreply.github.com>
2022-09-23 14:15:53 +0200

    Add files via upload

---
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("");
+        System.out.println("Hello World.");
+        System.out.println("I have no idea what I'm doing.");
+        System.out.println("");
+        System.out.println("     0     0");
+        System.out.println("");
+        System.out.println("      ----'      ");
+    }
+}
